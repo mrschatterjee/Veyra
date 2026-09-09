261654fa86018e2d2aead744473e85a3f1e61a40
mrschatterjee <mrschatterjee03@gmail.com>
2026-09-09 12:13:14 +0530

    Restore nudges after reboot

--- app/src/main/java/com/veyra/app/BootReceiver.kt
+++ app/src/main/java/com/veyra/app/BootReceiver.kt
@@ -6,8 +6,9 @@ import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED && VeyraReminder.isEnabled(context)) {
-            VeyraReminder.schedule(context, VeyraReminder.hour(context), VeyraReminder.minute(context))
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (VeyraReminder.isEnabled(context)) VeyraReminder.schedule(context, VeyraReminder.hour(context), VeyraReminder.minute(context))
+            NudgeScheduler.rescheduleAll(context)
         }
     }
 }
