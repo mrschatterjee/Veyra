727994fd1592c661d30cc1b3c68a75a9ce2f9f68
mrschatterjee <mrschatterjee03@gmail.com>
2026-09-09 22:43:28 +0530

    Add Firebase task coroutine bridge

---
+++ app/src/main/java/com/veyra/app/TaskExtensions.kt
@@ -0,0 +1,11 @@
+package com.veyra.app
+
+import com.google.android.gms.tasks.Task
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.suspendCancellableCoroutine
+
+suspend fun <T> Task<T>.awaitResult(): T = suspendCancellableCoroutine { continuation ->
+    addOnSuccessListener { value -> if (continuation.isActive) continuation.resume(value) }
+    addOnFailureListener { error -> if (continuation.isActive) continuation.resumeWithException(error) }
+}
