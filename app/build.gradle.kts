a21b91586a5f6fff4a529dcb3ff2fe9995cb5273
mrschatterjee <mrschatterjee03@gmail.com>
2026-09-09 20:38:43 +0530

    Align Veyra app version with current feature set

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.veyra.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
